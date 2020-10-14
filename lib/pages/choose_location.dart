5cac79b6c77cad25d92b871cd7cf81cd770826a7
Jackson Lazaro <jacksonlayaroict@gmail.com>
2020-10-14 03:37:07 -0700

    Updating WorldTime App

---
+++ lib/pages/choose_location.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class ChooseLocation extends StatefulWidget {
+  @override
+  _ChooseLocationState createState() => _ChooseLocationState();
+}
+
+class _ChooseLocationState extends State<ChooseLocation> {
+  void getData() {
+
+    //simulate network request for a username
+    Future.delayed(Duration(seconds: 3), () {
+      print('Jack');
+    });
+  }
+  @override
+
+  void initState() {
+    super.initState();
+    getData();
+
+  }
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('Choose a Location'),
+        centerTitle: true,
+        elevation: 0,
+      ),
+
+    );
+  }
+}
