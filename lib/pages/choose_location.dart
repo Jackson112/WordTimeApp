62d6475741a382a81af30d27161d50f4ab709027
Jackson Lazaro <jacksonlayaroict@gmail.com>
2020-10-20 11:56:25 -0700

    Choose location page adjustments

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wordtimeapp/services/world_time.dart';
 
 class ChooseLocation extends StatefulWidget {
   @override
@@ -6,7 +7,16 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
-
+ List<WorldTime> locations = [
+   WorldTime(url: 'Africa/Nairobi', location: 'Tanzania', flag: 'tanzania.jpg'),
+   WorldTime(url: 'Europe/London', location: 'London', flag: 'england.jpg'),
+   WorldTime(url: 'Europe/Berlin', location: 'Berlin', flag: 'german.png'),
+   WorldTime(url: 'America/Chicago', location: 'Chicago', flag: 'america.png'),
+   WorldTime(url: 'Africa/Cairo', location: 'Cairo', flag: 'cairo.jpg'),
+   WorldTime(url: 'America/Newyork', location: 'Newyork', flag: 'america.png'),
+   WorldTime(url: 'Asia/Jakarta', location: 'Jakarta', flag: 'indonesia.jpg' ),
+   WorldTime(url: 'Africa/Nairobi', location: 'Nairobi', flag: 'kenya.png' ),
+ ];
 
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,6 +26,21 @@ class _ChooseLocationState extends State<ChooseLocation> {
         title: Text('Choose A Location'),
         centerTitle: true,
         elevation: 0,
+      ),
+      body: ListView.builder(
+        itemCount:locations.length,
+        itemBuilder:(context,  index) {
+          return Card(
+            child: ListTile(
+              onTap: () {},
+              title: Text(locations[index].location),
+              leading: CircleAvatar(
+                backgroundImage: AssetImage('assets/${locations[index].flag}'),
+              ),
+
+            ),
+          );
+        },
       ),
     );
   }
--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'dart:convert';
 import 'package:wordtimeapp/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 class Loading extends StatefulWidget {
   @override
   _LoadingState createState() => _LoadingState();
@@ -34,10 +35,13 @@ class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(50.0),
-        child: Text('loading'),
-      )
-    );
+      backgroundColor: Colors.blue[900],
+      body: Center(
+        child: SpinKitSquareCircle(
+          color: Colors.white,
+          size: 50.0,
+        ),
+        ),
+      );
   }
 }
